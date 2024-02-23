75a0df6912346e97185f8c4f171d78729b02a1de
Mikola <das19901990a@gmail.com>
2024-02-23 13:21:59 +0200

    forma

---
+++ js/main.js
@@ -0,0 +1,25 @@
+const menuBTN = document.querySelector('.menu-btn');
+const menuMobile = document.querySelector('.menu-mobile');
+
+menuBTN.addEventListener('click', () => {
+  menuMobile.classList.toggle('menu--open');
+});
+
+const swiper = new Swiper('.swiper', {
+  loop: true,
+
+  navigation: {
+    nextEl: '.swiper-button-right',
+    prevEl: '.swiper-button-left',
+  },
+  breackpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 16,
+    },
+  },
+  // And if we need scrollbar
+});
